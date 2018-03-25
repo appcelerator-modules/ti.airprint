cd8849d9f30228dc6eeebb8e93a76f740ac06209
hansemannn <hans.knoechel@hs-osnabrueck.de>
2018-03-25 15:30:48 +0200

    Refactor module

--- ios/Classes/TiAirprintModuleAssets.h
+++ ios/Classes/TiAirprintModuleAssets.h
@@ -2,8 +2,7 @@
  * This is a generated file. Do not edit or your changes will be lost
  */
 
-@interface TiAirprintModuleAssets : NSObject
-{
+@interface TiAirprintModuleAssets : NSObject {
 }
-- (NSData*) moduleAsset;
+- (NSData *)moduleAsset;
 @end
